6d7260e3d3b8bb12389ed77a6c18ece82c6c5430
Darsh Thakur <thakurdarsh21@gmail.com>
2026-03-20 23:36:39 +0530

    grpc to implement accordingly

---
+++ src/main/java/com/uws/transaction_service/grpc/WalletServiceGrpcClient.java
@@ -0,0 +1,12 @@
+package com.uws.transaction_service.grpc;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public class WalletServiceGrpcClient {
+    public void debit(UUID senderId, BigDecimal amount, UUID transactionId, String s) {
+    }
+
+    public void credit(UUID senderId, BigDecimal amount, UUID transactionId, String s) {
+    }
+}
