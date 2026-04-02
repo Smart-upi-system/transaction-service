025597517a20beac87faa93f1d44b65838229295
Darsh Thakur <thakurdarsh21@gmail.com>
2026-04-02 21:20:52 +0530

    changes

--- src/main/java/com/uws/transaction_service/controller/TransactionController.java
+++ src/main/java/com/uws/transaction_service/controller/TransactionController.java
@@ -38,35 +38,35 @@ public class TransactionController {
         TransactionResponse response=transactionService.tranfer(
                 senderId ,request,correlationId);
 
-    return ResponseEntity.status(HttpStatus.CREATED).body(response);
-}
-
-@GetMapping("/{transactionId}")
-    public ResponseEntity<TransactionResponse> getTransaction(@RequestHeader("X-User-Id") String senderId, @PathVariable String transactionId){
-    log.info("GET /transactions/{}", transactionId);
-    TransactionResponse response=transactionService.getTransaction(senderId,transactionId);
-//   if (!response.getSenderId().equals(UUID.fromString(userId)) &&
-//            !response.getReceiverId().equals(UUID.fromString(userId))) {
-//            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
-//        }
-    if(ObjectUtils.isEmpty(response)){
-        return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
+
+    @GetMapping("/{transactionId}")
+        public ResponseEntity<TransactionResponse> getTransaction(@RequestHeader("X-User-Id") String senderId, @PathVariable String transactionId){
+        log.info("GET /transactions/{}", transactionId);
+        TransactionResponse response=transactionService.getTransaction(senderId,transactionId);
+    //   if (!response.getSenderId().equals(UUID.fromString(userId)) &&
+    //            !response.getReceiverId().equals(UUID.fromString(userId))) {
+    //            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+    //        }
+        if(ObjectUtils.isEmpty(response)){
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
+        return ResponseEntity.ok(response);
+
+    }
+
+    @GetMapping("/history")
+    public ResponseEntity<TransactionHistoryResponse> getHistory(@RequestHeader("X-User-Id") String userId,
+                                                                 @RequestParam(defaultValue = "0") int page,
+                                                                 @RequestParam(defaultValue = "20") int size){
+        log.info("GET /transactions/history - userId: {}", userId);
+
+        TransactionHistoryResponse response=transactionService.getTransactionHistory(userId, PageRequest.of(page,size));
+
     return ResponseEntity.ok(response);
 
-}
-
-@GetMapping("/history")
-public ResponseEntity<TransactionHistoryResponse> getHistory(@RequestHeader("X-User-Id") String userId,
-                                                             @RequestParam(defaultValue = "0") int page,
-                                                             @RequestParam(defaultValue = "20") int size){
-    log.info("GET /transactions/history - userId: {}", userId);
-
-    TransactionHistoryResponse response=transactionService.getTransactionHistory(userId, PageRequest.of(page,size));
-
-return ResponseEntity.ok(response);
-
-}
+    }
     /**
      * Get transaction state log (audit)
      */
